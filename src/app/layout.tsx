4e2db618206deb3c27048f04cc6179b2aa9ace05
YugBandhara <ybandhara@gmail.com>
2025-07-23 23:39:33 +0530

    Bug : Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import '@/app/globals.css';
 import { ReactNode } from 'react';
 import { DataGridProvider } from '@/contexts/DataGridContext';
 import './globals.css'
-import '../lib/i18n';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { useEffect } from 'react';
 import { useApi } from '../hooks/useAPi';
 import { useDataGridContext } from '@/contexts/DataGridContext';
 import DataGrid from '@/components/DataGrid/DataGrid';
-import '../lib/i18n';
-import { useTranslation } from 'react-i18next';
+
+
 export default function Page() {
-  const { t } = useTranslation();
+
   const { data, loading, error } = useApi('/api/users');
   const { dispatch } = useDataGridContext();
 
@@ -40,7 +40,7 @@ export default function Page() {
 
   return (
     <main className="p-4">
-      <h1 className="text-2xl font-bold mb-4">{t('Custom Data Grid')}</h1>
+      <h1 className="text-2xl font-bold mb-4">Custom Data Grid</h1>
 
       {loading && <p>Loading...</p>}
       {error && <p className="text-red-500">Error: {error}</p>}
